12d1414a92e623c7b0758f7b93e020fc47886434
박준형 <108733795+krapli441@users.noreply.github.com>
2023-06-04 12:37:56 +0900

    이해 쉽게 내용 변경

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,4 @@
 const header = document.createElement("h1");
-header.innerText = "안녕하세요, h1입니다.";
+header.innerText = "안녕하세요, header입니다.";
 
 export default header;
--- src/components/main.js
+++ src/components/main.js
@@ -1,4 +1,4 @@
 const main = document.createElement("h2");
-main.innerText = "안녕하세요, 저는 h2입니다.";
+main.innerText = "안녕하세요, 저는 main입니다.";
 
 export default main;
